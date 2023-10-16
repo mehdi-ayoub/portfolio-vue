9db03caff3ebc9f804068fae4b0166d490d698bd
mehdi ayoub <ayoub.mehdi@outlook.com>
2023-10-16 23:00:21 +0400

    added the image

---
+++ src/components/MainCards.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="header">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #080808;
+    color: #fff;
+  }
+
+  #header {
+    width: 100%;
+    height: 100vh;
+    background-image: url(../assets/photo-new.png);
+    background-size: cover;
+    background-position: center;
+  }
+</style>
